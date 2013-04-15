df4bc7f7af989705aa591178955fc97421071d9a
Jayant Sethi <thejayantsethi@gmail.com>
2013-04-15 16:54:00 +0530

    Education website

---
+++ application/views/home/bp_index.php
@@ -0,0 +1,355 @@
+
+<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en-US"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en-US"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en-US"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en-US"> <!--<![endif]-->
+
+<head>
+  
+  <!-- Meta Tags -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="author" content="Sauraabh Mehta, Jayant Sethi">
+  <meta name="description" content="Builing a better India! Educating India">
+  
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0;">
+
+  <!-- Humans.txt -->
+  <link type="text/plain" rel="author" href="/humans.txt" />
+  
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="/img/favicaon.ico" />
+  
+    
+  <!-- Title -->
+  <title> Educate India</title>
+  
+  <!-- Included CSS Files (Compressed) -->
+  <link rel="stylesheet" type="text/css" media="all" href="http://blog.thinkcerca.com/wp-content/themes/thinkcerca/css/foundation.min.css" />
+    
+  <!-- Included CSS Files (Uncompressed) -->
+  <link rel="stylesheet" type="text/css" media="all" href="http://blog.thinkcerca.com/wp-content/themes/thinkcerca/style.css?1363961090" />
+  
+  <!-- RSS & Pingbacks 
+  <link rel="alternate" type="application/rss+xml" title="ThinkCERCA RSS2 Feed" href="http://blog.thinkcerca.com/feed/" />  
+  <link rel="pingback" href="http://blog.thinkcerca.com/xmlrpc.php" />
+  -->
+  
+  <!-- Typekit 
+  <script type="text/javascript" src="//use.typekit.net/xbk5twl.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  -->
+  
+  <!-- Required header hook -->
+  <meta name='robots' content='noindex,nofollow' />
+<link rel="alternate" type="application/rss+xml" title="ThinkCERCA &raquo; Home Comments Feed" href="http://blog.thinkcerca.com/home/feed/" />
+<link rel='stylesheet' id='contact-form-7-css'  href='http://blog.thinkcerca.com/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=3.4' type='text/css' media='all' />
+<link rel='stylesheet' id='new-royalslider-core-css-css'  href='http://blog.thinkcerca.com/wp-content/plugins/new-royalslider/lib/royalslider/royalslider.css?ver=3.0.83' type='text/css' media='all' />
+<link rel='stylesheet' id='rsDefaultInv-css-css'  href='http://blog.thinkcerca.com/wp-content/plugins/new-royalslider/lib/royalslider/skins/default-inverted/rs-default-inverted.css?ver=3.0.83' type='text/css' media='all' />
+<script type='text/javascript' src='http://blog.thinkcerca.com/wp-includes/js/comment-reply.min.js?ver=3.5.1'></script>
+<script type='text/javascript' src='http://blog.thinkcerca.com/wp-includes/js/jquery/jquery.js?ver=1.8.3'></script>
+<script type='text/javascript' src='http://blog.thinkcerca.com/wp-content/themes/thinkcerca/js/foundation/modernizr.foundation.js?ver=1'></script>
+<script type='text/javascript' src='http://blog.thinkcerca.com/wp-content/themes/thinkcerca/js/html5.js?ver=1'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://blog.thinkcerca.com/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://blog.thinkcerca.com/wp-includes/wlwmanifest.xml" /> 
+<link rel='prev' title='Check out the list of lessons currently in our InstaCERCAs library!' href='http://blog.thinkcerca.com/instacercalist/' />
+<link rel='next' title='Platform' href='http://blog.thinkcerca.com/platform/' />
+<link rel='canonical' href='http://blog.thinkcerca.com/' />
+
+		<style media="print" type="text/css">
+			div.faq_answer {display: block!important;}
+			p.faq_nav {display: none;}
+		</style>
+
+	  
+</head>
+
+<body class="home page page-id-79 page-template page-template-page-homepage-php">
+
+  <header>
+    <!-- Fixed topbar -->     
+    <nav class="top-bar fixed">
+      <div class="row">
+      	<div class="four columns">
+      	</div>
+        <div class="four columns">
+              <ul>
+                <li><a class="brand" href="http://blog.thinkcerca.com"><img src="/img/logo.png" /></a></li>
+                <li class="toggle-topbar"><a href="#"></a></li>
+              </ul>                                      
+        </div>
+        <div class="four columns">
+      	</div>
+      </div>
+    </nav>
+    <!-- End topbar -->
+  </header>
+  
+      <div class="slider"><div id="new-royalslider-1" class="royalSlider new-royalslider-1 rsDefaultInv rs-default-template" style="width:100%; height:;"><div class="rsContent">
+  <img class="rsImg" src="http://blog.thinkcerca.com/wp-content/uploads/2013/02/home12.png" alt="Think Critical. Think Collaboration. ThinkCERCA!"/>
+  
+  
+  <div class="abTextAlignCenter rsABlock" style="width: 100%; height: 0px; left: 0px; top: 20%;" data-speed="" data-move-offset="" data-delay=""><h1>Builing a better India! Educating India.</h1>
+<h2>Prepare your students for the 21st century where anything is possible.<br>Teach your students how to think critically, engage with peers through the process of<br>academic argumentation, and build the writing skills they'll need to succeed in college and their careers.</h2></div>
+</div><div class="rsContent">
+  <img class="rsImg" src="http://blog.thinkcerca.com/wp-content/uploads/2013/03/home2.png" alt="Think Critical. Think Collaboration. ThinkCERCA!"/>
+  
+  
+  <div class="abTextAlignCenter rsABlock" style="width: 100%; height: 0px; left: 0px; top: 5%;" data-speed="" data-move-offset="" data-delay="" data-move-effect="top"><h1>Webinar: EducateINDIA Overview</h1>
+<h2 style="font-weight:bold;" class="dontHide">April 24, 4:00 CDT  <a href="https://attendee.gotowebinar.com/register/9040283909290042112" target="_blank" class="button round">Register</a></h2>
+<h2>Learn how EducateINDIA can be used to increase critical thinking through a career and<br>college readiness literacy program that works across disciplines.</h2> 
+<ul>
+  <li>Overview of framework &amp; research base</li>
+    <li>Introduction to the platform features &amp; lesson library</li>
+  <li>Using EducateINDIA in your school</li>
+</ul>
+</div>
+</div></div></div>
+    <div class="row" id="mainContent"><!-- Main row -->    <div class="twelve columns">
+            <div class="signup">
+<div class="twelve columns orangeBg">
+<h2>Sign Up for Launch Updates</h2>
+<p><!-- Begin MailChimp Signup Form --></p>
+<div id="mc_embed_signup" class="mailchimpForm">
+<form action="http://thinkcerca.us5.list-manage.com/subscribe/post?u=8de8cdefa1ef1b0ba88fedf99&amp;id=aa2d2d1878" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+<div class="mc-field-group">
+	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME" placeholder="First Name">
+</div>
+<div class="mc-field-group">
+	<input type="text" value="" name="LNAME" class="required" id="mce-LNAME" placeholder="Last Name">
+</div>
+<div class="mc-field-group">
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="E-mail address">
+</div>
+<div class="mc-field-group styledselect">
+<select name="MMERGE3" class="required" id="mce-MMERGE3">
+<option value="Educator" selected="selected">I am an Educator</option>
+<option value="Student">I am a Student</option>
+<option value="Administrator">I am an Administrator</option>
+<option value="Other">Other</option>
+</select>
+</div>
+<div class="mc-field-group">
+	<input type="text" value="" placeholder="Institution" name="MMERGE4" class="required" id="mce-MMERGE4">
+</div>
+<div id="mce-responses" class="clear">
+<div class="response" id="mce-error-response" style="display:none"></div>
+<div class="response" id="mce-success-response" style="display:none"></div>
+</p></div>
+<div class="clear"><input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" class="button round btnOrange"></div>
+</form>
+</div>
+<p><!--End mc_embed_signup--></div>
+</div>
+<div class="twelve columns">
+<div class="bigQuote"><span>&ldquo;</span>Education is what is left after one has forgotten everything one learned at school.<span>&rdquo;</span><br />
+<span class="author">Albert Einstein</span>
+</div>
+</div>
+<div class="clearfix"></div>
+<div class="break"></div>
+<div class="contentRow">
+<div class="twelve columns">
+<h2>What is Educate<span class="red">I</span><span class="yellow">N</span><span class="orange">D</span><span class="green">I</span><span class="blue">A</span>&trade;?</h2>
+</div>
+<div class="four columns">
+<h3 class="orange center"><img src="http://blog.thinkcerca.com/wp-content/uploads/2013/02/ic_method.png" alt="ic_method" width="189" height="189" class="aligncenter size-full wp-image-68" /><br />
+A Method</h3>
+<p>Our focus on argumentation as a method for teaching critical thinking is a simple way to increase rigorous engagement with all sorts of texts. Simply by treating all texts as arguments we allow readers to take an analytic approach.
+</p></div>
+<div class="four columns">
+<h3 class="green center"><img src="http://blog.thinkcerca.com/wp-content/uploads/2013/02/ic_platform.png" alt="ic_platform" width="189" height="189" class="aligncenter size-full wp-image-69" /><br />
+A Platform</h3>
+<p>Our platform allows teachers and students to collaborate more effectively by sharing a common language and set of practices that focus instruction on critical thinking.
+</p></div>
+<div class="four columns">
+<h3 class="blue center"><img src="http://blog.thinkcerca.com/wp-content/uploads/2013/02/ic_library.png" alt="ic_library" width="189" height="189" class="aligncenter size-full wp-image-70" /><br />
+A Library</h3>
+<p>EducateINDIA&trade; allows teachers to collaborate more easily by providing a simple framework for sharing, using, and personalizing lessons. Our content library is CCSS aligned and engaging for students.
+</p></div>
+<div class="clearfix"></div>
+</div>
+<div class="break"></div>
+<div class="contentRow">
+<div class="twelve columns">
+<h2>How Educate<span class="red">I</span><span class="yellow">N</span><span class="orange">D</span><span class="green">I</span><span class="blue">A</span>&trade; Works</h2>
+</div>
+<div class="four columns">
+<p>
+Teachers can design and deliver their own lessons but can also simply use lessons from our library, which offers a growing selection of lessons designed to meet the needs and interests of all students. Having done it for decades ourselves, we know that teachers don&#8217;t use things as they come out of the box, so our &#8220;Personalize&#8221; button works like a &#8220;Save As,&#8221; so teachers can adjust content to match the reality of their own classrooms without losing rigor. Teachers can also track data on student Common Core State Standards progress by assessing student work with our expert-aligned library.<br/><br />
+<a class="button round" href="teacher-resource-center">Visit our Teacher Resource Center</a></p>
+</div>
+<div class="eight columns">
+<div class="flex-video widescreen vimeo"><iframe src="http://player.vimeo.com/video/57761319?title=0&amp;byline=0&amp;portrait=0" width="640" height="360" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>
+</div>
+<div class="clearfix"></div>
+</div>
+<div class="break"></div>
+<div class="contentRow">
+<div class="twelve columns">
+<h2>Benefits</h2>
+</div>
+<div class="four columns">
+<h4 class="red">1. Personalize &#038; Differentiate</h4>
+<p>Student needs differ. Personalize our lessons or create your own for online delivery so you can deliver live student support while everyone is learning at their own pace.
+</p></div>
+<div class="four columns">
+<h4 class="blue">2. Track Student &#038; School Progress</h4>
+<p>Teaching &#038; learning cannot always be measured by standardized tests alone. By tracking authentic student work, teachers, adminstrators, and parents can see growth.
+</p></div>
+<div class="four columns">
+<h4 class="green">3. Coordinate Instruction Across Grade Levels &#038; Disciplines</h4>
+<p>Without a shared language, students find themselves confused as they move across disciplines. CERCA is a shared language that simplifies the core of CCSS.
+</p></div>
+<div class="clearfix"></div>
+<p><br/></p>
+<div class="twelve columns">
+<dl class="tabs contained mobile">
+<dd class="active"><a href="#create">Create Lessons</a></dd>
+<dd><a href="#track">Track Progress</a></dd>
+<dd><a href="#ccss">CCSS Alignment</a></dd>
+<dd><a href="#lesson">Lesson Library</a></dd>
+<dd><a href="#collab">Collaboration</a></dd>
+</dl>
+<ul class="tabs-content contained">
+<li class="active" id="createTab"><img src="http://blog.thinkcerca.com/wp-content/uploads/2013/02/Create-Lessons.png" alt="Create-Lessons" width="383" height="255" /><br />
+<h4>Creating Lessons is Easy</h4>
+<p>Teachers are no longer tethered to the front of the classroom. By uploading any text and using our tools, teachers can create CCSS aligned lessons for online delivery.
+<ul>
+<li>Upload content in any media format</li>
+<li>Select critical literacy tasks</li>
+<li>Deliver to students</li>
+</ul>
+<div class="clearfix"></div>
+</li>
+<li id="trackTab"><img src="http://blog.thinkcerca.com/wp-content/uploads/2013/02/Track-Progress.png" alt="Track-Progress" width="383" height="228" /><br />
+<h4>Track Progress</h4>
+<p>Real-time data empowers teachers and school leaders to monitor student progress, track students’ response to intervention and work as a team to meet students&#8217; needs.
+<ul>
+<li>Share common rubrics across a school</li>
+<li>See critical literacy growth</li>
+<li>Track CCSS achievement</li>
+</ul>
+<div class="clearfix"></div>
+</li>
+<li id="ccssTab"><img src="http://blog.thinkcerca.com/wp-content/uploads/2013/02/CCSS-alignment.png" alt="CCSS-alignment" width="383" height="215" /><br />
+<h4>CCSS Alignment</h4>
+<p>All ThinkCerca™ lessons meet CCSS standards by delivering content in engaging and rigorous ways. Create your own lessons and tie to relevant CCSS standards.
+<ul>
+<li>Library of CCSS aligned rubrics</li>
+<li>Tag literacy tasks with additional assessments (ACT &#038; NWEA)</li>
+<li>Tag text complexity</li>
+</ul>
+<div class="clearfix"></div>
+</li>
+<li id="lessonTab"><img src="http://blog.thinkcerca.com/wp-content/uploads/2013/02/Lesson-Library.png" alt="Lesson-Library" width="383" height="254" /><br />
+<h4>Lesson Library</h4>
+<p>Address the needs and interests of every student with a robust, modular library of engaging standards-aligned critical thinking lessons.
+<ul>
+<li>Download from growing selection directly to your account</li>
+<li>Lessons, not just lesson plans, are deliverable straight to students</li>
+<li>Personalize expert-created content – easy as &#8220;save as&#8221;</li>
+</ul>
+<div class="clearfix"></div>
+</li>
+<li id="collabTab"><img src="http://blog.thinkcerca.com/wp-content/uploads/2013/02/Collaboration.png" alt="Collaboration" width="383" height="215" /><br />
+<h4>Collaboration</h4>
+<p>
+<ul>
+<li>Teacher dashboard to track student learning across subjects</li>
+<li>Parent logins to participate in their child&#8217;s progress</li>
+<li>Peer-to-peer learning through interactive lessons</li>
+</ul>
+<div class="clearfix"></div>
+</li>
+</ul>
+</div>
+</div>
+<div class="clearfix"></div>
+<p><br/></p>
+<div class="break"></div>
+<div class="contentRow">
+<div class="twelve columns">
+<p style="text-align:center; font-size:18px; color:#666;">
+<a href="http://www.theimpactengine.com/impact-engine-companies" target="_blank"><img style="margin-right:10px; vertical-align:middle;" src="http://blog.thinkcerca.com/wp-content/uploads/2013/03/ImpactEngine.gif" alt="Impact Engine" width="136" height="68" />Impact 1</a><br />
+<a href="http://sxswedu.com/launchedu/finalists" target="_blank"><img style="margin-right:10px; vertical-align:middle;" src="http://blog.thinkcerca.com/wp-content/uploads/2013/02/sxsw_logo.png" alt="sxsw_logo" width="75" height="63" />2013 SXSW LAUNCHedu Finalist</a></p>
+</div>
+</div>
+                </div> 
+    </div>
+  
+</div><!-- main row -->
+  
+  <footer>
+    <div class="row">
+      <div class="three columns"></div>
+      <div class="three columns">
+        <p>
+          <ul>
+            <li class="facebook"><a href="https://www.facebook.com" target="_blank">Like Us on Facebook</a></li>
+            <li class="twitter"><a href="https://twitter.com" target="_blank">Follow Us on Twitter</a></li>
+          </ul>
+        </p>
+      </div>
+      <div class="three columns">
+        <img src="/img/footer_logo.png" />
+        <p>&copy; 2013. EducateINDIA. All Rights Reserved.</p>
+      </div>
+      <div class="three columns"></div>
+    </div>
+  </footer>
+  
+  <!-- Included JS Files (Compressed) -->
+  <script src="http://blog.thinkcerca.com/wp-content/themes/thinkcerca/js/foundation/foundation.min.js" type="text/javascript"></script>
+    
+  <!-- Initialize JS Plugins -->
+  <script src="http://blog.thinkcerca.com/wp-content/themes/thinkcerca/js/foundation/app.js" type="text/javascript"></script>
+  
+  <!-- Feedback form -->
+<script id="_webengage_script_tag" type="text/javascript">
+  window.webengageWidgetInit = window.webengageWidgetInit || function(){
+    webengage.init({
+      licenseCode:"~c2ab379a"
+    }).onReady(function(){
+      webengage.render();
+    });
+  };
+
+  (function(d){
+    var _we = d.createElement('script');
+    _we.type = 'text/javascript';
+    _we.async = true;
+    _we.src = (d.location.protocol == 'https:' ? "//ssl.widgets.webengage.com" : "//cdn.widgets.webengage.com") + "/js/widget/webengage-min-v-3.0.js";
+    var _sNode = d.getElementById('_webengage_script_tag');
+    _sNode.parentNode.insertBefore(_we, _sNode);
+  })(document);
+</script>
+  
+<script type='text/javascript' src='http://blog.thinkcerca.com/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=3.4'></script>
+<script type='text/javascript' src='http://blog.thinkcerca.com/wp-content/plugins/new-royalslider/lib/royalslider/jquery.royalslider.min.js?ver=3.0.83'></script>
+<script id="new-royalslider-init-code" type="text/javascript">
+jQuery(document).ready(function($) {
+	$('.new-royalslider-1').royalSlider({template:'default',image_generation:{imageWidth:1100,imageHeight:365,thumbImageWidth:'',thumbImageHeight:''},thumbs:{thumbWidth:96,thumbHeight:72},autoPlay:{enabled:!0,delay:4000},video:{vimeoCode:'<iframe src=\'http:\/\/player.vimeo.com\/video\/%id%?byline=0&amp;portrait=0&amp;autoplay=1\' frameborder=\'no\' webkitAllowFullScreen mozallowfullscreen allowFullScreen><\/iframe>'},block:{moveOffset:20,speed:400,delay:200},width:'100%',height:'',autoScaleSlider:!0,autoScaleSliderWidth:1100,autoScaleSliderHeight:365,slidesSpacing:0,imageScaleMode:'fit',imageScalePadding:0,arrowsNavAutoHide:!1});
+});
+</script>
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-36314020-1']);
+	  _gaq.push(['_setDomainName', 'thinkcerca.com']);
+	  _gaq.push(['_setAllowLinker', true]);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+
+</body>
+</html>
